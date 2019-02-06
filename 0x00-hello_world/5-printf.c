2b9a730dc67827867e85df0d08d68e1dd3f874ed
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-06 21:38:20 +0000

    Adding C program for Task#5 - printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
