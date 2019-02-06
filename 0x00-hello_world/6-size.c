1f2ed5bc0da2ec33a4df89dfc81a53d4a2bcea48
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-06 23:19:56 +0000

    Fixing code fot Task#6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(c));
 	printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(i));
-	printf("Size of an long int: %u byte(s)\n", (unsigned int)sizeof(l));
-	printf("Size of an long long int: %u byte(s)\n", (unsigned int)sizeof(d));
-	printf("Size of an float: %u byte(s)\n", (unsigned int)sizeof(f));
+	printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(l));
+	printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(d));
+	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(f));
 	return (0);
 }
