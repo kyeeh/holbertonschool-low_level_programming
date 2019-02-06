477004e57ece733aad11002a1d6857b23ca6058d
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-06 19:40:10 +0000

    Adding Shell Script to preprocess main.c

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,5 @@
+** Low Level Programming Project - C**
+
+**Task #0** - How to pre-process a C file.
+
+
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
