b3df76a25080ec5df96f1c945ca1cdd724d03837
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-07 21:55:10 +0000

    Printing hexa digits - Task#08

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; ++i)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
