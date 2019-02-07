e9bed30f5a9c6a9ce7cdac53fe1d1690c722d102
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-07 20:09:50 +0000

    Working with putchar and for loop for Task#02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
