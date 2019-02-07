c381a16cf01c756c090a6bdd384fc3fea06f9085
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-07 21:05:10 +0000

    Printing numbers as chars - Task#06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
