ffab0a32d3d1274ccdbbfb09103a173da0a1e903
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-08 04:40:20 +0000

    Fixing function on script for Task#09

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length;
+	length = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n", length);
 	return (1);
 }
