fe109d9cb65c4aeac0fb26bd7c586eea3bab5a5c
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-08 04:50:08 +0000

    Fixing errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,9 @@
  */
 int main(void)
 {
-	int length;
-	length = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", length);
+	int s;
+
+	s = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 	return (1);
 }
