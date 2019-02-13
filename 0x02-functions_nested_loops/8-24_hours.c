1f1aa2c18e6c9ab90ccc24e26af9510a7a3ffee2
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-13 03:03:15 +0000

    Fixing code for Task#08

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,8 +12,10 @@ void jack_bauer(void)
 
 	for (h1 = 0; h1 <= 2; h1++)
 	{
-		for (h2 = 0; h2 <= 3; h2++)
+		for (h2 = 0; h2 <= 9; h2++)
 		{
+			if (h1 > 1 && h2 > 3)
+				break;
 			for (m1 = 0; m1 <= 5; m1++)
 			{
 				for (m2 = 0; m2 <= 9; m2++)
