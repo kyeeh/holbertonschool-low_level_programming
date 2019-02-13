ce21bd75973ba76b920a4696614f121cbaeb82b0
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-13 04:21:49 +0000

    Adding C program for Task#10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - addition for two numbers
+ * @a: the integer "a"
+ * @b: the integer "b"
+ *
+ * Return: a + b.
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
