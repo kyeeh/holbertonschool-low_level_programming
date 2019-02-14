c84dc7c2ed6830bfe4199cbf869016fcf435c454
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-14 19:49:50 +0000

    Adding code for Tas#03

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print one digit numbers
+ *
+ * Return: Integer
+ */
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 0; 0 <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n')
+	return (0);
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -17,5 +17,6 @@ void print_times_table(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif /* HOLBERTON_H_ */
