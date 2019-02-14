1ac4a5c1807ac3eff0ec2c8a32c4af3d87f59746
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-14 19:31:24 +0000

    Adding code for Task#2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Two numbers multiplication
+ * @a: operator one.
+ * @b: operator two.
+ *
+ * Return: Multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -16,4 +16,6 @@ void print_to_98(int n);
 void print_times_table(int n);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+
 #endif /* HOLBERTON_H_ */
