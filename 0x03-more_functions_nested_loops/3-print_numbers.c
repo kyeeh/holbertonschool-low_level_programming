cba2724101d1617aea12a883138064219cf6d3f8
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-14 20:00:46 +0000

    Fixing code for Task#03

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * print_numbers - Print one digit numbers
  *
- * Return: Integer
  */
-int print_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; 0 <= 9; i++)
+	for (i = 0; i <= 9; i++)
+	{
 		_putchar(i + '0');
-	_putchar('\n')
-	return (0);
+	}
+	_putchar('\n');
 }
