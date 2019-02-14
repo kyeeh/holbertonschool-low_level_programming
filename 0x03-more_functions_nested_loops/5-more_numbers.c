f442ba85b287290297724da0600b4c83b1232034
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-14 21:01:14 +0000

    Adding C program for Task#05

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Print numbers in several lines.
+ *
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar(1 + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+}
