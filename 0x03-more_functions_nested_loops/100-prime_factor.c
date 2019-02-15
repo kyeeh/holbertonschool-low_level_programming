d3f719b5b02531fcf51ba2a09b0edf55880cdb37
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-15 01:18:22 +0000

    Fixing prim number program

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -10,19 +10,16 @@ int main(void)
 {
 	int i = 2;
 	double n = 612852475143;
-	/*int n = 1231952;*/
 
 	while (n > 0)
 		if (fmod(n, i) == 0)
 		{
-			printf("%d, ", i);
 			n = n / i;
 			i = 2;
 		}
 		else
 		{
 			i++;
-			/* printf("N=%i, i=%i",n,i);*/
 			if (i == n)
 			{
 				printf("%d\n", i);
