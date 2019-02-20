48f8f5e473683adab04d6ea629afa349b48c983c
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-20 04:45:19 +0000

    Fixing code for Task#09

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * _strcpy - copy a string
  * @dest: Destination String pointer.
@@ -15,6 +14,6 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
-	dest[++i] = '\0';
+	/* dest[++i] = '\0'; */
 	return (dest);
 }
