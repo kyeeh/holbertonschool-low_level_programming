b7f3531b9823c6ff9c0943f28d8943bf8e2b799d
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-22 05:11:11 +0000

    Fixing code for Task#08

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -8,11 +8,11 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ulphabet[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char ulphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
-		for (j = 0; j < 54; j ++)
+		for (j = 0; j < 52; j ++)
 			if (s[i] == alphabet[j])
 				s[i] = ulphabet[j];
 	return (s);
