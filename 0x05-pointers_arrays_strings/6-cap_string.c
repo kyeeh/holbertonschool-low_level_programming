f5303f275482871eee85e528ccdf4431e2a6f17a
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-22 01:13:43 +0000

    Adding code for Task#06

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,37 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * cap_string - capitalize words in a string
- * @s: the string to be checked
+ * isSeparator - checks if a char is separator
+ * @c: the char to be checked
  *
- * Return: a uppercase string pointer.
+ * Return: 0 or 1.
  */
-
-const char S[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\'', '(', ')', '{', '}' }; /* 12 SEPARATORS */
-
 int isSeparator(char c)
 {
+	/* 12 SEPARATORS */
+	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+				'\'', '(', ')', '{', '}' };
 	int i;
 
 	for (i = 0; i < 12; i++)
 	{
-		if (S[i] == c)
+		if (separators[i] == c)
 			return (1);
 	}
 	return (0);
 }
 
+/**
+ * cap_string - capitalize words in a string
+ * @s: the string to be checked
+ *
+ * Return: a uppercase string pointer.
+ */
 char *cap_string(char *s)
 {
 	int i;
 
 	for (i = 1; s[i] != '\0'; i++)
-		(isSeparator(s[i - 1]) && ((s[i] >= 97) && (s[i] <= 122))) ? s[i] -= 32 : 0 ;
+		(isSeparator(s[i - 1]) && ((s[i] >= 97) && (s[i] <= 122))) ? s[i] -= 32 : 0;
 	return (s);
 }
