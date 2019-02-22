d86aea11ade59131d41bde31e97697c6dab9fbbc
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-22 04:28:12 +0000

    Adding code for Task#7

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: the string to be checked
+ *
+ * Return: a encoded string pointer.
+ */
+char *leet(char *s)
+{
+	int i,j;
+
+	char c1encoder[] = {'t', 'a', 'e', 'l', 'o'};
+	char c2encoder[] = {'T', 'A', 'E', 'L', 'O'};
+	int  i_encoder[] = {7, 4, 3, 1, 0};
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 5; j++)
+			if ((s[i] == c1encoder[j]) || (s[i] == c2encoder[j]))
+				s[i] = i_encoder[j] + '0';
+	return (s);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -41,5 +41,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 
 #endif /* HOLBERTON_H_ */
