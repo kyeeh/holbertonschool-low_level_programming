0de85a67d105293d429329cae1955e5741240786
Ricardo Gutiérrez <ricardo@k2rstudio.com>
2019-02-26 09:36:10 -0500

    Fixing QA for tests

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, n;
-	
+
 	n = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -13,5 +13,5 @@ void print_chessboard(char (*a)[8])
 		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 		_putchar('\n');
-	}	
+	}
 }
