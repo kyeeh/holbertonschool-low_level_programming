818716e6616b0d392f01e96240e997ddfc5b7ae1
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-02-25 22:59:35 -0500

    Fixign QA for betty at Task#04

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -9,12 +9,10 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		for (j = 0; accept[j] != '\0'; j++)
 			if (s[i] == accept[j])
-				return(&s[i]);
-	}
+				return (&s[i]);
 	return (0);
 }
