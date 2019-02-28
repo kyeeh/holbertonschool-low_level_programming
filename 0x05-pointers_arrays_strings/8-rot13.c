2f6d51f81d62eb0a567d5c3854036a88d03a75e2
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-02-28 04:42:43 +0000

    Fixing code

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -12,7 +12,7 @@ char *rot13(char *s)
 	char ulphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
-		for (j = 0; j < 52; j ++)
+		for (j = 0; j < 52; j++)
 			if (s[i] == alphabet[j])
 				s[i] = ulphabet[j];
 	return (s);
--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -11,7 +11,7 @@ int str_checker(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
-	if (s1[i] == s2[j])		
+	if (s1[i] == s2[j])
 		return (str_checker(s1, s2, i + 1, j + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (str_checker(s1, s2, i, j + 1));
