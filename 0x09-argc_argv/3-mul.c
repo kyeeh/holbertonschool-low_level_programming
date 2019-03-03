04dec0b8353c7106cee8c0b4cc36e97c41f883b6
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-03 18:10:41 -0500

    Adding code for task#03

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,17 +1,21 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - check the code for Holberton School students.
  * @argc: The size of the argv array.
  * @argv: An array containing the program command line arguments
  *
- * Return: Always 0.
+ * Return: Always 0 || 1.
  */
 int main(int argc, char **argv)
 {
-	int i = 1;
-
-	for (; i <= argc; i++)
-		printf("%s\n", *argv++);
+	if (argc != 3)
+	{
+		printf("%s\n", "Error\n");
+		return (1);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
