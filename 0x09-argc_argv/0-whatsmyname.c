00937b48bc0dcffa7d6d2b3ced933dc390d98354
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-03 16:36:10 -0500

    Adding solution for Task#00

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - Prints a string in reverse, followed by a new line.
+ * @s: the string to be printed
+ *
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (argc);
+}
