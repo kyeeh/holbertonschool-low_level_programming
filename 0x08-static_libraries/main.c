15f8825f22ef03da8ad60c2f97f83d7dc07097e3
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-03-03 19:10:47 +0000

    Fixing betty QA

--- 0x08-static_libraries/main.c
+++ 0x08-static_libraries/main.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
