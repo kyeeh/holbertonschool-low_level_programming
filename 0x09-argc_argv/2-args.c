ab0ca9bf85d28eb640384cc0e37cf79cd1435cbc
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-03 17:37:17 -0500

    Adding solution for Task#02

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: The size of the argv array.
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i = 1;
+
+	for (; i <= argc; i++)
+		printf("%s\n", *argv++);
+    return (0);
+}
