03fcc3c182f63e5fb12737b1a1ea88678ca7136c
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-03 17:04:00 -0500

    Fixing QA Tests

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  * main - check the code for Holberton School students.
- *
+ * @argc: The size of the argv array.
+ * @argv: An array containing the program command line arguments
+ * 
  * Return: Always argc.
  */
 int main(int argc, char **argv)
