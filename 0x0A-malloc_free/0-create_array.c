741cc5608865dbd528f615e3e1140987f4d652e3
Ricardo Gutiérrez <ricardo@k2rstudio.com>
2019-03-05 15:35:52 -0500

    Fixing code for tests

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array = malloc(size * sizeof(char *));
 
+	if (size == 0 || array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);
