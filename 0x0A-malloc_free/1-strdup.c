5b5d6ff3c9095251a83cbe4e09d9f3edb9acb37e
Ricardo Gutiérrez <ricardo@k2rstudio.com>
2019-03-05 16:37:36 -0500

    Fixing code for testing

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -10,14 +10,17 @@
 char *_strdup(char *str)
 {
 	unsigned int i = 0, size = 0;
-
-	while (str[size] != '\0')
-		size++;
-	char *array = malloc(size * sizeof(char));
-
-	if (str == NULL || array == NULL)
+	
+	if (str == NULL)
 		return (NULL);
-	for (; i < size; i++)
-		array[i] = str[i];
+	else
+		while (str[size] != '\0')
+			size++;
+		char *array = malloc(size * sizeof(char));
+
+		if (array == NULL)
+			return (NULL);
+		for (; i < size; i++)
+			array[i] = str[i];
 	return (array);
 }
