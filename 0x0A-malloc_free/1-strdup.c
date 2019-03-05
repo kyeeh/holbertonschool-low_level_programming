a0b90e796459f75b57eb4c428d4063e37ffa9600
Ricardo Gutiérrez <ricardo@k2rstudio.com>
2019-03-05 16:05:15 -0500

    Adding code for Task#02

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -10,11 +10,11 @@
 char *_strdup(char *str)
 {
 	unsigned int i;
-	char *array = malloc(size * sizeof(char *));
+	char *array = malloc(size * sizeof(char));
 
-	if (size == 0 || array == NULL)
+	if (str == NULL || array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		array[i] = c;
+		array[i] = str[i];
 	return (array);
 }
