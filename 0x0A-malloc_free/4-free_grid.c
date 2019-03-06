08ec0e2c558369dd21cd97bbac6aa80a6f374eee
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-05 23:29:17 -0500

    Adding scaffold for Task#04

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -8,22 +8,12 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i, j, **matrix = NULL;
-
-	if (height <= 0 || height <= 0)
-		return (NULL);
-	matrix = malloc(sizeof(height * sizeof(int *)));
-	if (matrix == NULL)
-		return (NULL);
+	int i, j;
 	for (i = 0; i < height; i++)
 	{
-		matrix[i] = malloc(height * sizeof(int));
-		if (matrix[i] == NULL)
-			return (NULL);
 		for (j = 0; j < height; j++)
 		{
-			matrix[i][j] = 0;
+			grid[i][j] = 0;
 		}
 	}
-
 }
