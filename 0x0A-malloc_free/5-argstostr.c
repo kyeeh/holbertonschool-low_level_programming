7b91d299d447c7b55764f4f31e0b4478fa2e9009
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-06 23:27:50 -0500

    Fixing code for Task#05

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * argstostr - concatenates arguments.
  * @ac: number of string to be concatenated.
@@ -10,28 +9,23 @@
 char *argstostr(int ac, char **av)
 {
 	int i, j, k = 0;
-	char *array = NULL, *concat = NULL;
+	char *concat = NULL;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	array = malloc(sizeof(char) * ac * 100);
-	if (array == NULL)
-		return (NULL);
 	for (i = 0; i < ac; i++)
-	{
 		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			array[k] = av[i][j];
 			k++;
-		}
-		array[k] = '\n';
+	concat = malloc(sizeof(char) * (k + 1));
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0, k = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			concat[k] = av[i][j];
+		concat[k] = '\n';
 		k++;
-		printf("K: %i, AC: %i\n", k, ac);
 	}
-	concat = malloc(sizeof(char) * (++k));
-	concat = array;
 	concat[k] = '\0';
-	array = NULL;
-	free(array);
 	return (concat);
 }
