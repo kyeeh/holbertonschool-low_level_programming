810ea06f11ef54f94f233fefe62c0ada799ddd7b
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-11 15:10:16 -0500

    Adding code for Task#00

---
+++ 0x0C-preprocessor/README.md
@@ -0,0 +1,15 @@
+#### PREPROCESOR
+
+**Task #00** - defines a macro named SIZE as an abbreviation for the token 1024.
+
+**Task #01** - Concatenates two strings.
+
+**Task #02** - Allocates memory for an array.
+
+**Task #03** - Returns a pointer to a 2-dimensional array of integers.
+
+**Task #04** - Frees a 2 dimensional grid.
+
+**Task #05** - Concatenates all the arguments of your program.
+
+**Task #06** - Splits a string into words.
---
+++ 0x0C-preprocessor/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif /* HOLBERTON_H_ */
