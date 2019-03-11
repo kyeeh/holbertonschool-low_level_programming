ca684ed23a5fe77b40b148a947e0262494dd0aef
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-03-11 23:02:46 +0000

    Fixing code for Task#00

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,8 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Dog definition
+ * @name: dog name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Dog Definition
+ */
 typedef struct dog dog;
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
