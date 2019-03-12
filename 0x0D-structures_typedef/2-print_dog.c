7bce4dd7c74f074c0dc132c7567e3e203eb37053
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-11 23:58:41 -0500

    Adding code for Task#02

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - prints a dog data structure.
+ * @d: dog pointer.
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("\n");
+	if (d->name == NULL)
+		printf("(nil)\n");
+	else
+		printf("Name: %s\n",d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("Owner: %s\n",d->owner);
+}
