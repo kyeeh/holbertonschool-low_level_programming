a28b69a4a0c5e0330ddfc2196c0e1fd02a7b0890
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-12 00:06:21 -0500

    Fixing header

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -9,13 +9,17 @@
  *
  * Description: Dog Definition
  */
-typedef struct dog dog;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
