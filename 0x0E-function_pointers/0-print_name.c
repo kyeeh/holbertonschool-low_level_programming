a349bf6b6499b335011e57847d20be9d57465a21
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-13 23:16:04 -0500

    Adding code for Task#00

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name.
+ * @name: name to print.
+ * @f: function to print name.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTERS_H_
+#define FUNCTION_POINTERS_H_
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H_ */
