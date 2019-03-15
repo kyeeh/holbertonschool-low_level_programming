6659063cc1944c95165f20753e9b8f262f873c03
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-14 19:00:32 -0500

    Fixing errors for Task#01 - validations

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -10,6 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	for (i = 0; i < (int)size; i++)
-		action(array[i]);
+	if (array && action)
+		for (i = 0; i < (int)size; i++)
+			action(array[i]);
 }
