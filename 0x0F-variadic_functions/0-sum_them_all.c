7152731fcb70ba2fdee60e277eb0a3c5aaefd2e1
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-16 14:53:18 -0500

    Adding code for Task#01

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -8,7 +8,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-    va_list a_list;
+	va_list a_list;
 
 	va_start(a_list, n);
 	if (n == 0)
--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -8,7 +8,7 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-    va_list a_list;
+	va_list a_list;
 
 	va_start(a_list, n);
 	for (i = 0; i < n - 1; i++)
