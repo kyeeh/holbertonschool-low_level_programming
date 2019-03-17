a94297181e2d19c537aba1ff25fe64f1689a91de
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-16 21:51:20 -0500

    Fixing code for advanced Task#04

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,18 +1,17 @@
-global _start
+global main
 
 section .text
-
-_start:
+main:
   mov rax, 1        ; write(
   mov rdi, 1        ;   STDOUT_FILENO,
   mov rsi, msg      ;   "Hello, Holberton\n",
-  mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
+mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
   syscall           ; );
 
   mov rax, 60       ; exit(
   mov rdi, 0        ;   EXIT_SUCCESS
   syscall           ; );
 
-section .rodata
+section .data
   msg: db "Hello, Holberton", 10
   msglen: equ $ - msg
