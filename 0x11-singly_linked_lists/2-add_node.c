ac5318f249b10c345c1810489c2680e47f4d10be
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-21 23:45:15 -0500

    Adding code for strleng

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -2,6 +2,20 @@
 #include <string.h>
 #include "lists.h"
 /**
+ * _strlen - calculated the string length.
+ * @str: pointer to String to be checked.
+ * Return: string length.
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int length = 0;
+
+	while(str[length])
+		length++;
+	return (length);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list.
  * @head: pointer to list head.
  * @str: pointer to String to be included in the new node.
@@ -17,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		if (str)
 		{
-			(*head)->len = strlen(str);
+			(*head)->len = _strlen(str);
 			(*head)->str = strdup(str);
 		}
 		else
