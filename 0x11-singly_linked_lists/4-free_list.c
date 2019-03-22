cf93698d62f794b53035c78767d20aa20dc649c5
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-22 00:00:04 -0500

    Adding code for Task#04

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * _strlen - calculated the string length.
+ * @str: pointer to String to be checked.
+ * Return: string length.
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int length = 0;
+
+	while (str[length])
+		length++;
+	return (length);
+}
+
+/**
+ * last_node - return the last_node of a list.
+ * @h: pointer to list.
+ * Return: last node.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp_node = head;
+
+	while (tmp_node)
+		tmp_node = tmp_node->next;
+	return (tmp_node);
+}
+/**
+ * add_node_end - adds a new node at the beginning of a list_t list.
+ * @head: pointer to list head.
+ * @str: pointer to String to be included in the new node.
+ * Return: list of nodes.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp_node;
+
+	tmp_node = last_node(*head); /* Address of last node */
+	*head = malloc(sizeof(list_t));
+	if (*head)
+	{
+		if (str)
+		{
+			(*head)->len = _strlen(str);
+			(*head)->str = strdup(str);
+		}
+		else
+		{
+			(*head)->len = 0;
+			(*head)->str = "(nil)";
+		}
+		(*head)->next = tmp_node; /* New Head points to old_head*/
+	}
+	return (*head);
+}
