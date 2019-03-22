a760fb89035d0ff3c393d012326b9ed127d219db
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-21 22:44:10 -0500

    Adding code for Task#00

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list -  prints all the elements of a list_t list.
+ * @h: pointer to list.
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int n = 0;
+	const list_t *tmp_node = h;
+
+	while (tmp_node)
+	{
+		if (tmp_node->str)
+			printf("[%u] %s\n", tmp_node->len, tmp_node->str);
+		else
+			printf("[0] (nil)\n");
+		tmp_node = tmp_node->next;
+		n++;
+	}
+	return (n);
+}
---
+++ 0x11-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - linked list
+ * @str: string pointer to store node data.
+ * @len: string length.
+ * @next: pointer to next node.
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
