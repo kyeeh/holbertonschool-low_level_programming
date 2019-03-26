60bafa0c4300c2f504a82b34ff43907bd16be49a
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-26 10:38:04 -0500

    Fixing code for Task#01

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * listint_len_rec - returns the number of elements in a linked
  * listint_t list recursively.
@@ -10,7 +9,7 @@
 size_t listint_len_rec(const listint_t *head, unsigned int n)
 {
 	if (head)
-		n = print_list_rec(head->next, n + 1);
+		n = listint_len_rec(head->next, n + 1);
 	return (n);
 }
 /**
