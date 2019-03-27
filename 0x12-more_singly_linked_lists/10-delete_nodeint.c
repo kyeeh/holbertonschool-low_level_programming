fae7a286fe37cbbb69f52fb327eb7f0b62411194
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-26 23:17:00 -0500

    Adding base cases

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *tmp_node;
 
 	new_node = malloc(sizeof(listint_t));
-	if (head && new_node)
+	if (new_node)
 	{
 		if (idx == 0 && *head == NULL)
 		{
