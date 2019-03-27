ce8b267f2bdb1bb67931756c19a8d3a1ed252e31
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-27 00:18:34 -0500

    Adding code for Task#103

--- 0x12-more_singly_linked_lists/103-find_loop.c
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -1,28 +1,12 @@
 #include "lists.h"
-
 /**
- * print_list_rec - prints all the elements of a listint_t list recursively.
+ * find_listint_loop - prints all the elements of a listint_t list in a save way.
  * @head: pointer to list.
- * @n: number of nodes acumlated as param.
- * Return: number of nodes.
+ * Return: list.
  */
-size_t print_list_rec(const listint_t *head, unsigned int n)
+listint_t *find_listint_loop(listint_t *head)
 {
 	if (head)
-	{
-		n = print_list_rec(head->next, n + 1);
-		printf("[%p] %i\n", (void *)head, head->n);
-	}
-	return (n);
-}
-/**
- * print_listint_safe - prints all the elements of a listint_t list in a save way.
- * @head: pointer to list.
- * Return: number of nodes.
- */
-size_t print_listint_safe(const listint_t *head)
-{
-	if (head)
-		return (print_list_rec(head, 0));
+		return (head);
 	exit(98);
 }
