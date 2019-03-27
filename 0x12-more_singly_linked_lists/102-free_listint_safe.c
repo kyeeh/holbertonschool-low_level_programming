e1e7a419ca8bbccf8d26965e54ef60ff23e72799
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-27 00:23:11 -0500

    Fixing Betty

--- 0x12-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,6 +3,7 @@
  * rec_free_listint - frees a listint_t list.
  * @head: pointer to list.
  * @size: list' size.
+ * Return: number of nodes.
  */
 int rec_free_listint(listint_t *head, int size)
 {
@@ -16,11 +17,12 @@ int rec_free_listint(listint_t *head, int size)
 /**
  * free_listint_safe - frees a listint_t list with double pointer.
  * @h: pointer to pointer to list.
+ * Return: number of nodes.
  */
 size_t free_listint_safe(listint_t **h)
 {
 	int size = 0;
-	
+
 	if (h && *h)
 	{
 		size = rec_free_listint(*h, size);
