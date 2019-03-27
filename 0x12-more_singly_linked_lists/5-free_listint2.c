95ab43ff76c51996cce50ba73690a21a9c6905cd
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-26 19:17:50 -0500

    Fixing code for Task#05

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -18,7 +18,7 @@ void free_listint(listint_t *head)
 void free_listint2(listint_t **head)
 {
 	if (head)
-    if (*head)
-      free_listint(*head);
-  *head = NULL;
+		if (*head)
+			free_listint(*head);
+	*head = NULL;
 }
