bb80570396abd036292af9d9c4c7a471ad51c1d5
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-26 21:21:25 -0500

    Adding code for Task#08

---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * sum_listint_rec - get the nth node of a listint_t linked list recursively.
+ * @head: pointer to list.
+ * @sum: sum of node's data.
+ * Return: sum.
+ */
+int sum_listint_rec(listint_t *head, int sum)
+{
+	if (head)
+		sum = sum_listint_rec( head->next, sum + head->n );
+	return (sum);
+}
+/**
+ * sum_listint - sum data of nodes on a listint_t linked list.
+ * @head: pointer to list.
+ * Return: the sum result.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head)
+		return (sum_listint_rec(head, sum));
+	return (0);
+}
