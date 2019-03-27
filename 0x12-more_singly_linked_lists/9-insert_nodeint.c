a8ba50399b46353a3bed5a250be5c62bd9001533
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-26 22:16:51 -0500

    Fixing code with struct init for Task#09

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,23 +31,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head)
 	{
 		new_node = malloc(sizeof(listint_t));
-		if (!new_node)
-			return (NULL);
-		if (*head)
+		if (new_node)
 		{
-			tmp_node = get_node_rec(*head, idx, i);
-			if (tmp_node)
+			new_node->n = n;
+			if (*head)
 			{
-				new_node->n = n;
-				new_node->next = tmp_node->next;
-				tmp_node->next = new_node;
+				tmp_node = get_node_rec(*head, idx, i);
+				if (tmp_node)
+				{
+					new_node->next = tmp_node->next;
+					tmp_node->next = new_node;
+					return (new_node);
+				}
+			}
+			else
+			{
+				new_node->next = NULL;
+				*head = new_node;
 				return (new_node);
 			}
-		}
-		else
-		{
-			*head = new_node;
-			return (new_node);
 		}
 	}
 	return (NULL);
