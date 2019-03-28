7a8c40be21fa190f1c10f34d9f5a7fb0de47644c
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-28 18:16:35 -0500

    Cleaning code for Task#00 0x13

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -9,28 +9,21 @@ unsigned int binary_to_uint_rec(const char *b, unsigned int *params)
 {
 	if (*b != '\0')
 	{
-		printf("String: %s\n", b);
-		printf("Valor de b[%i]: %c\n", params[1], *b);
 		if (*b != '0' || *b != '1')
 		{
 			params[1]++;
-			printf("SIGUIENTE Valor de b[%i]: %c\n", params[1], *(b + 1));
 			binary_to_uint_rec(b + 1, params);
 			params[0] += (*b - 48) * pow(2, params[1] - params[2]);
-			printf("CALCULO DECIMAL: %u\n", params[0]);
 			params[2]--;
-			printf("RETORNANDO: Param[1]: %u, Param[2]: %u\n", params[1], params[2]);
 			return (params[0]);
 		}
 		else
 		{
-			printf("ERROR: %s\n", b);
 			params[3] = 0;
 			return (0);
 		}
 	}
 	params[2] = params[1];
-	printf("SALIENDO: Param[1]: %u, Param[2]: %u\n", params[1], params[2]);
 	return (0);
 }
 /**
@@ -48,6 +41,5 @@ unsigned int binary_to_uint(const char *b)
 		if (params[3])
 			return (params[0]);
 	}
-	printf("---------------------\n");
 	return (0);
 }
