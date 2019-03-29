964882a3b9dc6d3a9e3df5475d0b570fb7f5dd73
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-28 21:35:27 -0500

    Adding code for Task#04 0x13

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * set_bit -  sets the value of a bit to 1 at a given index.
+ * @n: decimal number.
+ * @index: of bit to be return.
+ * Return: selected new decimal.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1 << index;
+
+	if (index <= (sizeof(n) * 8) - 1)
+	{
+		*n &= ~mask;
+		return (1);
+	}
+	return (-1);
+}
