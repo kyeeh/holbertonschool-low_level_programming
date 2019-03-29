832387122b2510f5daa3747c11b3032acf594fde
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-28 19:20:52 -0500

    Fixing code with bitwise calculation

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,20 @@
-#include <math.h>
 #include "holberton.h"
 /**
  * binary_to_uint_rec - binary to decimal recursively.
  * @b: pointer to string of 0 ir 1.
- * @params: array of auxiliar params for decimal calculation
+ * @dec: decimal number for calculation.
  * Return: decimal number.
  */
-unsigned int binary_to_uint_rec(const char *b, unsigned int *params)
+unsigned int binary_to_uint_rec(const char *b, unsigned int dec)
 {
 	if (*b != '\0')
-	{ /* params = {decimal, number_of_chars, current_index, error_flag} */
+	{
 		if (*b >= 48 && *b <= 49) /* ONLY BINARIES */
-		{
-			params[1]++;
-			binary_to_uint_rec(b + 1, params);
-			params[0] += (*b - 48) * pow(2, params[1] - params[2]);
-			params[2]--;
-			return (params[0]);
-		}
+			return (binary_to_uint_rec(b + 1, (dec << 1) | (*b - 48)));
 		else
-		{
-			params[3] = 0;
 			return (0);
-		}
 	}
-	params[2] = params[1];
-	return (0);
+	return (dec);
 }
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
@@ -34,13 +23,9 @@ unsigned int binary_to_uint_rec(const char *b, unsigned int *params)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int params[5] = {0, 0, 0, 1};
+	unsigned int dec = 0;
 
 	if (b)
-	{
-		binary_to_uint_rec(b, params);
-		if (params[3])
-			return (params[0]);
-	}
+		return (binary_to_uint_rec(b, dec));
 	return (0);
 }
