1522aa1862b05214f8d1648d5e3a1710998446d7
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-04-02 23:55:45 -0500

    Adding code for Task#100

--- 0x14-file_io/100-elf_header.c
+++ 0x14-file_io/100-elf_header.c
@@ -1,27 +1,14 @@
+#include<stdio.h>
 #include "holberton.h"
 /**
- * read_textfile - reads a text file and prints it to the standard output.
- * @filename: The file's name.
- * @letters: The file's length.
+ * main - main function
+ * @arc: number of arguments.
+ * @argv: pointer to array of arguments.
  * Return: 1 on sucess or -1 on failure.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int main(int argc, char *argv[])
 {
-	char *buffer = NULL;
-	int fdesc, size, error = -1;
-
-	if (filename)
-	{
-		fdesc = open(filename, O_RDONLY);
-		buffer = malloc(sizeof(char) * letters);
-		if (fdesc > 0 && buffer)
-		{
-			size = read(fdesc, buffer, letters);
-			if (size >= 0 && (size_t)size <= letters)
-				error = write(STDOUT_FILENO, buffer, size);
-			close(fdesc);
-		}
-	}
-	free(buffer);
-	return ((error >= 0 && error == size) ? error : 0);
+	printf("%i, %p\n", argc, (void *)*argv);
+	system("readelf -a");
+	return (1);
 }
