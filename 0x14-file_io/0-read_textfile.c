7285f1aab5a997a602de33320fe19360f78fef22
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-04-02 22:29:54 -0500

    Adding code for Task#00

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: The file's name.
+ * @letters: The file's length.
+ * Return: 1 on sucess or -1 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	unsigned int size;
+	int fdesc, error = -1;
+	char buffer[letters];
+
+	if (filename)
+	{
+		fdesc = open(filename, O_RDONLY);
+		if (fdesc > 0)
+		{
+			size = read(fdesc, &buffer, letters);
+			if(size <= letters)
+				error = write(1, &buffer, size);
+			close(fdesc);
+			return ((error >= 0) ? size : 0);
+		}
+	}
+	return (0);
+}
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -5,6 +5,7 @@
 #include<fcntl.h> 
 #include<unistd.h>
 
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 
 #endif /* HOLBERTON_H_ */
