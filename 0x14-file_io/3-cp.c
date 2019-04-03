de733ad0eb48026047cc7ed54cbea2d63842efd3
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-04-03 00:21:42 -0500

    Adding code for Task#03

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,26 +1,38 @@
+#include<stdio.h>
 #include "holberton.h"
 /**
- * read_textfile - reads a text file and prints it to the standard output.
- * @filename: The file's name.
- * @letters: The file's length.
+ * main - copies the content of a file to another file.
+ * @argc: number of arguments.
+ * @argv: pointer to array of arguments.
  * Return: 1 on sucess or -1 on failure.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int main(int argc, char *argv[])
 {
 	char *buffer = NULL;
-	int fdesc, size, error = -1;
+	int fd_origin, fd_destiny, buf_size = 1024, size, error = -1;
 
-	if (filename)
+	if (argc != 3)
 	{
-		fdesc = open(filename, O_RDONLY);
-		buffer = malloc(sizeof(char) * letters);
-		if (fdesc > 0 && buffer)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_origin  = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd_destiny = open(argv[2], O_TRUNC, 664);
+	if (fd_origin)
+	{
+		buffer = malloc(sizeof(char) * buf_size);
+		if (fd_origin > 0 && buffer)
 		{
-			size = read(fdesc, buffer, letters);
-			if (size >= 0 && (size_t)size <= letters)
+			size = read(fd_origin, buffer, buf_size);
+			if (size >= 0 && size <= buf_size)
 				error = write(STDOUT_FILENO, buffer, size);
-			close(fdesc);
+			close(fd_origin);
 		}
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(97);		
 	}
 	free(buffer);
 	return ((error >= 0 && error == size) ? error : 0);
