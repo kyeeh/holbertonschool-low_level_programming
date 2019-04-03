87233e4bc1c3fbad8a4b77a94f99d4b0f7ef8706
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-04-02 21:35:08 -0500

    Adding code for Task#01

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * create_file - creates a file.
+ * @filename: The file's name.
+ * @text_content: The file's content.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fdesc, error, size = 0;
+
+	if (filename)
+	{
+		fdesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fdesc > 0)
+		{
+			while (text_content && text_content[size] != '\0')
+				size++;
+			error = write(fdesc, text_content, size);
+			close(fdesc);
+			return (error > 0) ? 1 : -1;
+		}
+	}
+	return (-1);
+}
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+#include<stdio.h>
+#include<fcntl.h> 
+#include<unistd.h>
+
+int create_file(const char *filename, char *text_content);
+
+#endif /* HOLBERTON_H_ */
