a95e864f0b66f85033fd8cd3152c5d49331078fe
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-04-02 22:32:55 -0500

    Adding README to the project

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to the standard output.
  * @filename: The file's name.
  * @letters: The file's length.
  * Return: 1 on sucess or -1 on failure.
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if (fdesc > 0)
 		{
 			size = read(fdesc, &buffer, letters);
-			if(size <= letters)
+			if (size <= letters)
 				error = write(1, &buffer, size);
 			close(fdesc);
 			return ((error >= 0) ? size : 0);
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,11 @@
+#### Bit manipulation
+
+**Task #00** - Reads a text file and prints it to the POSIX standard output.
+
+**Task #01** - Creates a file.
+
+**Task #02** - Adds a new node at the beginning of a listint_t list.
+
+**Task #03** - Adds a new node at the end of a listint_t list
+
+**Task #04** - Frees a listint_t list.
