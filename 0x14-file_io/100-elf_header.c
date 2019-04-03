12a08bf7fac2dc4559dbd4f7d295c52f67d24b6e
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-04-02 23:58:37 -0500

    Fixing code

--- 0x14-file_io/100-elf_header.c
+++ 0x14-file_io/100-elf_header.c
@@ -2,13 +2,13 @@
 #include "holberton.h"
 /**
  * main - main function
- * @arc: number of arguments.
+ * @argc: number of arguments.
  * @argv: pointer to array of arguments.
  * Return: 1 on sucess or -1 on failure.
  */
 int main(int argc, char *argv[])
 {
 	printf("%i, %p\n", argc, (void *)*argv);
-	system("readelf -a");
+	system("readelf -a ubuntu64");
 	return (1);
 }
