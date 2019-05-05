bc55484f59fc188c4ce5e7b5f8f3961d260969c8
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-05 21:34:23 +0000

    Adding code for Task#01

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len_rec - number of elements in a linked dlistint_t list.
+ * @h: double linked list.
+ * @elements: number of nodes.
+ * Return: number of elements value.
+ */
+size_t dlistint_len_rec(const dlistint_t *h, size_t elements)
+{
+	if (h)
+		elements = dlistint_len_rec(h->next, elements + 1);
+	return (elements);
+}
+
+/**
+ * print_dlistint - get the number of elements of a dlistint_t list.
+ * @h: double linked list.
+ * Return: number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	return (dlistint_len_rec(h, 0));
+}
