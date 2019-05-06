3fa2a3656292e6b4d4668d7e0dd8a6f92c5967b9
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-06 04:58:32 +0000

    Fixing header delete

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,15 +10,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *bfr_node = *head, *tmp_node = *head;
 
-	if (index == 0)
+	if (index == 0 && *head)
 	{
-		if (*head)
+		if ((*head)->next)
 		{
 			*head = (*head)->next;
-			if (*head)
-				(*head)->prev = NULL;
+			(*head)->prev = NULL;
 			free(tmp_node);
 		}
+		else
+			*head = NULL;
 		return (1);
 	}
 	while (i < index - 1 && bfr_node)
@@ -30,12 +31,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		tmp_node = bfr_node->next;
 		if (tmp_node->next)
-		{
 			tmp_node->next->prev = bfr_node;
-			bfr_node->next = tmp_node->next;
-		}
-		else
-			bfr_node->next = NULL;
+		bfr_node->next = tmp_node->next;
 		free(tmp_node);
 		return (1);
 	}
