1eb99afbc7d0ea32bc7574f5154124051879de2b
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-06 03:36:59 +0000

    Fixing previous node for h

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new_node, *tmp_node = NULL;
+	dlistint_t *new_node, *tmp_node = *h;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (h && new_node)
@@ -18,13 +18,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (idx == 0)
 		{
 			if (*h)
+			{
 				new_node->next = *h;
+				(*h)->prev = new_node;
+			}
 			*h = new_node;
 			return (new_node);
 		}
 		if (*h)
 		{
-			tmp_node = *h;
 			while (i < idx - 1 && tmp_node)
 			{
 				tmp_node = tmp_node->next;
