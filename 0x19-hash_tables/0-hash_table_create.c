47d997dd39d273241467b613fd99a37c7c4d1c6b
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-31 23:38:30 +0000

    Adding code for Task#00

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of the array
+ *
+ * Return: On success pointer to hash_table_t.
+ * On error, NULL.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i;
+	hash_table_t *new_ht = NULL;
+
+	new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht && size > 0)
+	{
+		new_ht->size = size;
+		new_ht->array =  malloc(size * sizeof(hash_node_t));
+		if (!new_ht->array)
+		{
+			for (i = 0; i < size; i++)
+				new_ht->array[i] = NULL;
+			free(new_ht);
+		}
+	}
+	return (new_ht);
+}
