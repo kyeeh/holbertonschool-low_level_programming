0132d177e2d888784e431bfb03d621f8f29820d2
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-31 23:42:22 +0000

    Fixing betty and size validation

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,16 +12,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned int i;
 	hash_table_t *new_ht = NULL;
 
-	new_ht = malloc(sizeof(hash_table_t));
-	if (new_ht && size > 0)
+	if (size)
 	{
-		new_ht->size = size;
-		new_ht->array =  malloc(size * sizeof(hash_node_t));
-		if (!new_ht->array)
+		new_ht = malloc(sizeof(hash_table_t));
+		if (new_ht)
 		{
-			for (i = 0; i < size; i++)
-				new_ht->array[i] = NULL;
-			free(new_ht);
+			new_ht->size = size;
+			new_ht->array =  malloc(size * sizeof(hash_node_t));
+			if (!new_ht->array)
+			{
+				for (i = 0; i < size; i++)
+					new_ht->array[i] = NULL;
+				free(new_ht);
+			}
 		}
 	}
 	return (new_ht);
