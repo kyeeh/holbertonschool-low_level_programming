0562fbaf2fc160c3374d0a953ba84c2807c1331c
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-01 03:35:31 +0000

    Fixing ht validation

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -10,11 +10,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 
-	new_node = calloc(sizeof(hash_node_t), 1);
+	new_node = malloc(sizeof(hash_node_t));
 	if (ht && key && (key != '\0') && value && new_node)
 	{
+		index = key_index((const unsigned char *)key, ht->size);
 		new_node->key = strdup(key);
 		new_node->value = strdup(value);
 		new_node->next = ht->array[index];
