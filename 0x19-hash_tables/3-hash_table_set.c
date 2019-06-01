8f08b989c393af962542599f10ab69c5b0bcc29d
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-01 03:15:09 +0000

    Adding return 1

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -19,6 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node->value = strdup(value);
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
+		return (1);
 	}
 	return (0);
 }
