7946d350e07f12345b5249a9aeccb6dd25f54cd6
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-01 00:11:20 +0000

    Adding code for Task#01

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm.
+ * @str: str pointer.
+ *
+ * Return: index of hash_table_t.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+}
