668c7fad8556d488e036ed5302271f650c123213
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-01 03:42:37 +0000

    Fixing value validation

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,8 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	new_node = calloc(sizeof(hash_node_t), 1);
-	if (ht && key && (key != '\0') && value && new_node)
+	new_node = malloc(sizeof(hash_node_t));
+	if (ht && key && (key != '\0') && (value != NULL) && new_node)
 	{
 		index = key_index((const unsigned char *)key, ht->size);
 		new_node->key = strdup(key);
