21baf66410307069013481067168794724ecc009
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-01 04:33:04 +0000

    Adding code for Task#05

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	char *sep;
+	hash_node_t *tmp_node;
+	unsigned long int index;
+
+	if (ht && ht->array)
+	{
+		printf("{");
+		for (index = 0; index < ht->size; index++)
+		{
+			tmp_node = ht->array[index];
+			while (tmp_node)
+			{
+				printf("%s'%s': '%s'", sep, tmp_node->key, tmp_node->value);
+				tmp_node = tmp_node->next;
+				sep = ", ";
+			}			
+		}
+		printf("}\n");
+	}
+}
