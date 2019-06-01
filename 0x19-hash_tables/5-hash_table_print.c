c49f092efc37a5f57ba280901ced1e79865d7641
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-01 04:38:22 +0000

    Fixing Betty

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -20,7 +20,7 @@ void hash_table_print(const hash_table_t *ht)
 				printf("%s'%s': '%s'", sep, tmp_node->key, tmp_node->value);
 				tmp_node = tmp_node->next;
 				sep = ", ";
-			}			
+			}
 		}
 		printf("}\n");
 	}
