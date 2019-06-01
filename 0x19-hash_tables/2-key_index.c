7da7223d97ca8cabacef2133c5858a34521a343f
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-01 00:46:13 +0000

    Adding code for Task#02

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key.
+ * @key: pointer to key
+ * @size: size of the array
+ *
+ * Return: index to store the key/value pair.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
