99b9052c1ef3e1b1c2b85cbc9bc182bfa753db41
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-19 22:36:42 +0000

    Fixing base case

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,19 +1,6 @@
 #include "sort.h"
-/**
- * swap - swaps array elements a, b
- * @array: element's array
- * @i: index for "a" element
- * @j: index for "b" element
- */
-void swap(int *array, size_t i, size_t j)
-{
-	int temp;
 
-	temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
-
-}
+void swap(int *array, size_t i, size_t j);
 
 /**
  * bubble_sort - sorts an integer array using the Bubble sort algorithm
@@ -25,14 +12,28 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (size < 2)
-		return;
+	if (array && size >= 2)
+		for (i = 0; i < size - 1; i++)
+			for (j = 0; j < size - i - 1; j++)
+				if (array[j] > array[j + 1])
+				{
+					swap(array, j, j + 1);
+					print_array(array, size);
+				}
+}
 
-	for (i = 0; i < size - 1; i++)
-		for (j = 0; j < size - i - 1; j++)
-			if (array[j] > array[j + 1])
-			{
-				swap(array, j, j + 1);
-				print_array(array, size);
-			}
+/**
+ * swap - swaps array elements a, b
+ * @array: element's array
+ * @i: index for "a" element
+ * @j: index for "b" element
+ */
+void swap(int *array, size_t i, size_t j)
+{
+	int aux;
+
+	aux = array[i];
+	array[i] = array[j];
+	array[j] = aux;
+
 }
