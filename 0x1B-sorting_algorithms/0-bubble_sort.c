40987b2cc0f409cc3f936e99e7166e0f9b605921
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-19 23:08:32 +0000

    Cleaning code

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -13,19 +13,13 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 
 	if (array && size > 1)
-	{
 		for (i = 0; i < size - 1; i++)
-		{
-			for (j = 0; j < size - 1; j++)
-			{
+			for (j = 0; j < size - i - 1; j++)
 				if (array[j] > array[j + 1])
 				{
 					swap(array, j, j + 1);
 					print_array(array, size);
 				}
-			}
-		}
-	}
 }
 
 /**
