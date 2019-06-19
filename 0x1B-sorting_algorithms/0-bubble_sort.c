05f532f6fad346767a5f884df11ed7defdbe3c42
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-19 22:26:20 +0000

    Adding base case

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -25,6 +25,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < size - i - 1; j++)
 			if (array[j] > array[j + 1])
