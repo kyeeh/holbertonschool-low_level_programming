6e93ad8adc89c3a42f424c124fd1e6a2f2c13ee0
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-19 22:42:54 +0000

    Adding brackets

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -13,13 +13,19 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 
 	if (array && size >= 2)
+	{
 		for (i = 0; i < size - 1; i++)
+		{
 			for (j = 0; j < size - i - 1; j++)
+			{
 				if (array[j] > array[j + 1])
 				{
 					swap(array, j, j + 1);
 					print_array(array, size);
 				}
+			}
+		}
+	}
 }
 
 /**
