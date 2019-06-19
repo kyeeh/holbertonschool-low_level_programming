2c517911ce3da425d9dcfa3871c35c24fefbc58e
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-19 21:54:50 +0000

    Adding code for Task#00

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * swap - swaps array elements a, b
+ * @array: element's array
+ * @i: index for "a" element
+ * @j: index for "b" element
+ */
+void swap(int *array, size_t i, size_t j)
+{
+	int temp;
+
+	temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+
+}
+
+/**
+ * bubble_sort - sorts an integer array using the Bubble sort algorithm
+ * @array: array to sort
+ * @size: array's size
+ * Return: Nothing.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				swap(array, j, j + 1);
+				print_array(array, size);
+			}
+}
