612b70cb6c69033013aadc7b7efa9144d9343b38
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-20 04:37:44 +0000

    Adding code base for AdvTasks

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,5 +27,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif  /* SORT_H_ */
