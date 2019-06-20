9e6fec07b430394e95960635fa4fc7419bfc5509
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-20 04:11:41 +0000

    Adding base code for mandatories

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+void swap(int *array, size_t i, size_t j);
+
+/**
+ * selection_sort - sorts an integer array using the Selection sort algorithm
+ * @array: array to sort
+ * @size: array's size
+ * Return: Nothing.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (array && size > 1)
+		for (i = 0; i < size - 1; i++)
+			for (j = 0; j < size - i - 1; j++)
+				if (array[j] > array[j + 1])
+				{
+					swap(array, j, j + 1);
+					print_array(array, size);
+				}
+}
+
+/**
+ * swap - swaps array elements a, b
+ * @array: element's array
+ * @i: index for "a" element
+ * @j: index for "b" element
+ */
+void swap(int *array, size_t i, size_t j)
+{
+	int aux;
+
+	aux = array[i];
+	array[i] = array[j];
+	array[j] = aux;
+
+}
