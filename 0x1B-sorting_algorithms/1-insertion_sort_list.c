6ff11b7e45d25739aaeeaa840927028097266f34
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-20 00:03:41 +0000

    Adding code for Task#01

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * insertion_sort_list -  sorts a DLL of integers using the Insertion sort
+ * @list: DLL to sort
+ * Return: Nothing.
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+}
