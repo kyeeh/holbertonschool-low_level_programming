18b4569be2fcce1c8e6bcbde737aa8d48c97aaa9
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-07-08 04:05:14 +0000

    Adding base file for Task#05

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Island perimeter - Technical interview preparation
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of character island
+    """
+
+    return 12
